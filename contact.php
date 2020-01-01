90a86239caece80063332ad74947a4397c9cbb74
Dave Van Fleet <dvfleet413@gmail.com>
2020-01-01 15:12:35 -0500

    Finished styling and added favicon

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'markproclean@yahoo.com';
+$from = 'dvfleet413@gmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'markproclean@yahoo.com';
+$sendTo = 'dvfleet413@gmail.com, davidvanfleet@icloud.com, markproclean@yahoo.com';
 
 // subject of the email
 $subject = 'New message from contact form';
